2e2e76e9de682fb3259bb0fb08cf9ab57446f87b
vixadd <kroelld.96@gmail.com>
2022-06-19 17:26:47 +0000

    GIT #2: Adding updates to the websites interoperability.

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 // Server information and specification for nodejs
 
 const http = require('http');
+const express = require('express');
+const path = require('path');
 
 const hostname = '0.0.0.0';
 const port = 43881;
 
+/*
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
+*/
+
+
+const app = express();
+app.use(express.json());
+app.use(express.static("express"));// default URL for website
+app.use('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/dkroell/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+const server = http.createServer(app)
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
