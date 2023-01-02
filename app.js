e87800ab954dcacb79c64e61aaf67130045bf23a
vixadd <kroelld.96@gmail.com>
2023-01-02 17:38:13 -0500

    Re-formatting some of the elements in order

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const hostname = '0.0.0.0';
 const port = 43881;
+const app = express();
 
 /*
 const server = http.createServer((req, res) => {
@@ -15,26 +16,23 @@ const server = http.createServer((req, res) => {
 });
 */
 
-
-const app = express();
 app.use(express.json());
-app.use(express.static(__dirname + "/"));// default URL for website
-
-app.use('/', function(req,res){
+app.use(express.static(__dirname + '/public'));
+app.use('/', function(req,res) {
     res.sendFile(path.join(__dirname+'/dkroell/index.html'));
-    //__dirname : It will resolve to your project folder.
 });
 
-app.use('/font-awesome.css', function(req,res) {
-    res.sendFile(path.join(__dirname+'/node_modules/font-awesome/css/font-awesome.min.css'));
-});
+//app.use('/font-awesome.css', function(req,res) {
+//    res.sendFile(path.join(__dirname+'/node_modules/font-awesome/css/font-awesome.min.css'));
+//});
 
 app.use('/index.css', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dkroell/index.css'));
+    res.send("ok")
 });
 
-const server = http.createServer(app)
+//app.use('/static', express.static(path.join(__dirname, 'public')))
 
+const server = http.createServer(app)
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
