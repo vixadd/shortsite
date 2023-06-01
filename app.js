189b46df3989158108d573b6a94fb150e214cc24
vixadd <kroelld.96@gmail.com>
2023-06-01 08:27:35 -0400

    Adding port accessibility and change.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 43881; // Use the PORT environment variable, or 43881 if PORT is not set
@@ -10,10 +11,6 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/', function(req,res) {
     res.sendFile(path.join(__dirname+'/dkroell/index.html'));
-});
-
-app.use('/index.css', function(req,res) {
-    res.send("ok")
 });
 
 const server = http.createServer(app)
