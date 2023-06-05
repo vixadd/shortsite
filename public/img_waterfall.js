96d87fb5dc8d27e3013fb47e3f791cbdba4df7c0
vixadd <kroelld.96@gmail.com>
2023-06-05 15:27:57 -0400

    Fixing transparent background issue.

--- public/img_waterfall.js
+++ public/img_waterfall.js
@@ -66,9 +66,9 @@ image.addEventListener('load', function(){
     init();
 
     function animate () {
-        //ctx.globalAlpha = 0.05;
-        //ctx.fillStyle = 'rgb(0, 0,0)';
-        //ctx.fillRect(0, 0, canvas.width, canvas.height);
+        // ctx.globalAlpha = 0.05;
+        // ctx.fillStyle = 'rgb(0, 0,0)';
+        // ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.globalAlpha = 0.2;
         for (let i = 0; i < particlesArray.length; i++) {
             particlesArray[i].update();
